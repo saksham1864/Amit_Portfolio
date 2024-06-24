c1d6444e3c4152db7057e5c662a2a0aee15c1d7e
saksham sharma <132849795+saksham1864@users.noreply.github.com>
2024-06-24 19:51:08 +0530

    update

---
+++ components/nav.js
@@ -0,0 +1,49 @@
+// components/Navbar.js
+'use client'
+import { useState } from 'react';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="fixed top-0 left-0 w-full bg-gray-800 p-4 z-10">
+      <div className="flex justify-between items-center">
+        <div className="text-white text-lg font-bold">Soulful Living</div>
+        <div className="lg:hidden">
+          <button onClick={toggleMenu} className="text-white focus:outline-none">
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={!isOpen ? 'M4 6h16M4 12h16m-7 6h7' : 'M6 18L18 6M6 6l12 12'}></path>
+            </svg>
+          </button>
+        </div>
+        <div className={`lg:flex lg:items-center lg:w-auto ${isOpen ? 'block' : 'hidden'} w-full lg:w-auto`}>
+          <div className="text-sm lg:flex-grow">
+            <a href="#blogs" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4">
+              Blogs
+            </a>
+            <a href="#featured" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4">
+              Featured
+            </a>
+            <a href="#upload" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4">
+              Upload
+            </a>
+            <a href="#about" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400">
+              About
+            </a>
+          </div>
+          <div>
+            <button className="mt-4 lg:mt-0 bg-green-700 text-white py-2 px-4 rounded hover:bg-green-800">
+              My Profile
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
